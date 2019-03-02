5fe12283666013d405a3a320849985b8dc49794c
Dawid Barsznica <dbarsznica@gmail.com>
2019-03-02 12:59:35 +0100

    Add help

--- js/script.js
+++ js/script.js
@@ -60,9 +60,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 'li');
         
         this.element.querySelector('.card').addEventListener('click', function (event) {
-            event.stopPropagation();
-        
+            event.stopPropagation();     
             if (event.target.classList.contains('btn-delete')) {
+                self.removeCard();
+            }
+
+            if (event.target.classList.contains('btn-delete-card')) {
                 self.removeCard();
             }
         });
@@ -130,8 +133,43 @@ jQuery(".titleWrapper h1").each(function(){
 	$.each(characters, function (i, el) {
 		if(el == " "){el = "&nbsp;"};
     $this.append("<span>" + el + "</span");
-	});
-		
+	});		
+});	
 });
-	
+
+$(document).ready(function() {
+  
+    var content = $('.content');
+    var currentItem = content.filter('.active');
+    var steps = $('.card-help').filter('.steps');
+    var inactive1 = $('.inactive-1');
+    var inactive2 = $('.inactive-2');
+
+    $('.button').click(function() {
+        var nextItem = currentItem.next();
+        var lastItem = content.last();
+        var contentFirst = content.first();
+      
+        currentItem.removeClass('active');
+
+        if (currentItem.is(lastItem)) {
+            currentItem = contentFirst.addClass('active');
+            currentItem.css({'right': '10%', 'opacity': '1'});
+            $('.step').animate({width: '33%'});
+            inactive1.animate({height: '8px', marginLeft:'20px', marginRight:'20px'}, 100);
+            inactive2.animate({height: '8px', marginLeft:'10px', marginRight:'10px'}, 100);
+          
+        } else if (currentItem.is(contentFirst)) {
+            currentItem.animate({opacity: 0}, 1000);
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({width: '66%'});
+            inactive2.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
+          
+        } else {
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({width: '100%'});
+            inactive1.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
+        } 
+    });
+  
 });
