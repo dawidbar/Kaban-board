3b9059ca5fc48ffa41570beea617638c2f9f5b06
Dawid Barsznica <dbarsznica@gmail.com>
2019-03-02 16:41:41 +0100

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     function randomString() {
         var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
@@ -9,58 +9,58 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         return str;
     };
-    
+
     function generateTemplate(name, data, basicElement) {
         var template = document.getElementById(name).innerHTML;
         var element = document.createElement(basicElement || 'div');
-      
+
         Mustache.parse(template);
         element.innerHTML = Mustache.render(template, data);
-      
+
         return element;
     };
 
     function Column(name) {
         var self = this;
-      
+
         this.id = randomString();
         this.name = name;
-        this.element = generateTemplate('column-template', { 
-            name: this.name, 
-            id: this.id 
+        this.element = generateTemplate('column-template', {
+            name: this.name,
+            id: this.id
         });
-      
-        this.element.querySelector('.column').addEventListener('click', function (event) {
-          if (event.target.classList.contains('btn-delete')) {
-            self.removeColumn();
-          }
 
-          if (event.target.classList.contains('add-card')) {
-            self.addCard(new Card(prompt("Enter the name of the card")));
-          }
+        this.element.querySelector('.column').addEventListener('click', function (event) {
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeColumn();
+            }
+
+            if (event.target.classList.contains('add-card')) {
+                self.addCard(new Card(prompt("Enter the name of the card")));
+            }
         });
     }
 
     Column.prototype = {
-        addCard: function(card) {
+        addCard: function (card) {
             this.element.querySelector('ul').appendChild(card.element);
         },
-        removeColumn: function() {
+        removeColumn: function () {
             this.element.parentNode.removeChild(this.element);
         }
     };
 
     function Card(description) {
         var self = this;
-        
+
         this.id = randomString();
         this.description = description;
-        this.element = generateTemplate('card-template', { 
-            description: this.description 
+        this.element = generateTemplate('card-template', {
+            description: this.description
         }, 'li');
-        
+
         this.element.querySelector('.card').addEventListener('click', function (event) {
-            event.stopPropagation();     
+            event.stopPropagation();
             if (event.target.classList.contains('btn-delete')) {
                 self.removeCard();
             }
@@ -72,14 +72,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     Card.prototype = {
-        removeCard: function() {
+        removeCard: function () {
             this.element.parentNode.removeChild(this.element);
         }
     }
 
     var board = {
         name: 'Kanban Board',
-        addColumn: function(column) {
+        addColumn: function (column) {
             this.element.appendChild(column.element);
             initSortable(column.id);
         },
@@ -89,12 +89,12 @@ document.addEventListener('DOMContentLoaded', function() {
     function initSortable(id) {
         var el = document.getElementById(id);
         var sortable = Sortable.create(el, {
-          group: 'kanban',
-          sort: true
+            group: 'kanban',
+            sort: true
         });
     }
 
-    document.querySelector('#board .create-column').addEventListener('click', function() {
+    document.querySelector('#board .create-column').addEventListener('click', function () {
         var name = prompt('Enter a column name');
         var column = new Column(name);
         board.addColumn(column);
@@ -118,58 +118,87 @@ document.addEventListener('DOMContentLoaded', function() {
     todoColumn.addCard(card1);
     doingColumn.addCard(card2);
 
-    
+
 });
 
-jQuery(document).ready(function(){
-	jQuery(".titleWrapper").addClass("ready");
-	
-jQuery(".titleWrapper h1").each(function(){
-	var fullString;
-	var characters = jQuery(this).text().split("");
-	
-	var $this = jQuery(this);
-	$this.empty();
-	$.each(characters, function (i, el) {
-		if(el == " "){el = "&nbsp;"};
-    $this.append("<span>" + el + "</span");
-	});		
-});	
-});
+$(function () {
+    console.log("ready!");
 
-$(document).ready(function() {
-  
+    jQuery(document).ready(function () {
+        jQuery(".titleWrapper").addClass("ready");
+
+        jQuery(".titleWrapper h1").each(function () {
+            var fullString;
+            var characters = jQuery(this).text().split("");
+
+            var $this = jQuery(this);
+            $this.empty();
+            $.each(characters, function (i, el) {
+                if (el == " ") {
+                    el = "&nbsp;"
+                };
+                $this.append("<span>" + el + "</span");
+            });
+        });
+    });
+
     var content = $('.content');
     var currentItem = content.filter('.active');
     var steps = $('.card-help').filter('.steps');
     var inactive1 = $('.inactive-1');
     var inactive2 = $('.inactive-2');
 
-    $('.button').click(function() {
+    $('.button').click(function () {
         var nextItem = currentItem.next();
         var lastItem = content.last();
         var contentFirst = content.first();
-      
+
         currentItem.removeClass('active');
 
         if (currentItem.is(lastItem)) {
             currentItem = contentFirst.addClass('active');
-            currentItem.css({'right': '10%', 'opacity': '1'});
-            $('.step').animate({width: '33%'});
-            inactive1.animate({height: '8px', marginLeft:'20px', marginRight:'20px'}, 100);
-            inactive2.animate({height: '8px', marginLeft:'10px', marginRight:'10px'}, 100);
-          
+            currentItem.css({
+                'right': '10%',
+                'opacity': '1'
+            });
+            $('.step').animate({
+                width: '33%'
+            });
+            inactive1.animate({
+                height: '8px',
+                marginLeft: '20px',
+                marginRight: '20px'
+            }, 100);
+            inactive2.animate({
+                height: '8px',
+                marginLeft: '10px',
+                marginRight: '10px'
+            }, 100);
+
         } else if (currentItem.is(contentFirst)) {
-            currentItem.animate({opacity: 0}, 1000);
+            currentItem.animate({
+                opacity: 0
+            }, 1000);
             currentItem = nextItem.addClass('active');
-            $('.step').animate({width: '66%'});
-            inactive2.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
-          
+            $('.step').animate({
+                width: '66%'
+            });
+            inactive2.animate({
+                height: '0',
+                marginLeft: '0px',
+                marginRight: '0px'
+            }, 100);
+
         } else {
             currentItem = nextItem.addClass('active');
-            $('.step').animate({width: '100%'});
-            inactive1.animate({height: '0', marginLeft:'0px', marginRight:'0px'}, 100);
-        } 
+            $('.step').animate({
+                width: '100%'
+            });
+            inactive1.animate({
+                height: '0',
+                marginLeft: '0px',
+                marginRight: '0px'
+            }, 100);
+        }
     });
-  
 });
